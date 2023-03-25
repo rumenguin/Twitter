1a77bbb1b6ae4d22d8bca28650f46d2753bd3182
Rumen Guin <rumen2k@gmail.com>
2023-03-25 11:48:42 +0530

    create twitter project

--- TwitterClone/Controllers/Core/DirectMessagesViewController.swift
+++ TwitterClone/Controllers/Core/DirectMessagesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DirectMessagesViewController.swift
 //  TwitterClone
 //
 //  Created by RUMEN GUIN on 25/03/23.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DirectMessagesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
