f82105d64bd2d1a89d56b15db3223f5e305de371
Rumen Guin <rumen2k@gmail.com>
2023-03-25 10:42:26 +0530

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by RUMEN GUIN on 25/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
